6aea54a2584beaaf63d42e423ee301f058080f84
johnny_shaman <johnny_shaman@live.jp>
2019-02-12 17:32:46 +0000

    modified:   README.md 	modified:   cte.js 	modified:   package.json

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Category Theory Environment
 Enpowerment Prototype Object on algebraic and Category Theory
+
+Usage:
+
--- cte.js
+++ cte.js
@@ -2,568 +2,285 @@
   "use strict";
 
   let _ = function (v) {
+    return Object.create(_.prototype, {
+      "@": {
+        configurable: true,
+        get () {
+          return v;
+        }
+      }
+    });
   };
-
-  Object.assign(_, {
-    id_: v => v == null ? undefined : v.valueOf(),
-    is_: v => v == null ? "null" : v.name ? v.name : _.of_(v).name,
-    of_: v => v.constructor,
-    by_: v => v.prototype ? v.prototype: _.of_(v).prototype,
-    fullen_: a => !(a.includes(undefined) || a.includes(null)),
-    fill_: (n, v) => _.put_([], {length: n}).fill(v),
-    have_: (v, c) => v instanceof c,
-    sets_: Object.entries,
-    keys_: Object.keys,
-    vals_: Object.values,
-    put_:  Object.assign,
-    feat_: Object.create,
-    struct_: Object.defineProperties,
-    pipe_: (...f) => (...v) => _(f).foldR((a, f) => f(a), ...v),
-    use_:  (p, ...o) => _.put_(_.feat_(p), ...o),
-    Cat: v => _.feat_(
-      _.by_(_)[_.is_(v)]
-      ? _.by_(_)[_.is_(v)]
-      : _.by_(_).Category,
-      {
-        name: {
-          configurable: true,
-          value: "Cat"
-        },
-        "@": {
-          configurable: true,
-          writable: true,
-          value: v
-        },
-        "<": {
-          configurable: true,
-          writable: true,
-          value: undefined
-        },
-        cached: {
-          configurable: true,
-          writable: true,
-          value: undefined
-        }
-      }
-    )
-  });
-  _.put_(_.by_(_), {
-    Category: _.feat_(_.feat_(null, {
-      endo: {
-        configurable: true,
-        value (...f) {
-          return  _.Cat(_.pipe_(...f)(this.flat));
-        }
-      },
-      map: {
-        configurable: true,
-        value (...f) {
-          return _.Cat(_.pipe_(...f)(this.flat));
-        }
-      },
-      flip: {
-        configurable: true,
-        value (...f) {
-          return this.endo(...f).flat;
-        }
-      },
-      biject: {
-        configurable: true,
-        value (...f) {
-          return this.endo(t => f.map(g => g(t)));
-        }
-      },
-      struct: {
-        configurable: true,
-        value (o) {
-          return this.endo(t => _.struct_(t, o));
-        }
-      },
-      be: {
-        configurable: true,
-        value (...f) {
-          return this.peek.endo(t => _.pipe_(...f)(t) ? t : undefined);
-        }
-      },
-      is: {
-        configurable: true,
-        value (c) {
-          return this.be(o => _.is_(o) === c);
-        }
-      },
-      isnt: {
-        configurable: true,
-        value (c) {
-          return this.be(o => _.is_(o) !== c);
-        }
-      },
-      lift: {
-        configurable: true,
-        value (n) {
-          return _.Cat(_.struct_(this, {
-            name: {
-              configurable: true,
-              value: n
-            }
-          }));
-        }
-      },
-      unit: {
-        configurable: true,
-        get () {
-          return this.lift("Unit");
-        }
-      },
-      peek: {
-        configurable: true,
-        get () {
-          return this.unit.peek.flat;
-        }
-      },
-      poke: {
-        configurable: true,
-        get () {
-          return this.unit.poke.flat;
-        }
-      },
-      _: {
-        configurable: true,
-        get () {
-          return _.id_(this["@"]);
-        }
-      }
-    }))
-  });
-  _.put_(_.by_(_), {
-    Object: _.feat_(_.by_(_).Category, {
-      sets: {
-        configurable: true,
-        get () {
-          return this.endo(_.sets_);
-        }
-      },
-      keys: {
-        configurable: true,
-        get () {
-          return this.endo(_.keys_);
-        }
-      },
-      vals: {
-        configurable: true,
-        get () {
-          return this.endo(_.vals_);
-        }
-      },
-      fold: {
-        configurable: true,
-        value (...f) {
-          return this.sets.endo(
-            s => s.reduce(
-              _.pipe_(...f),
-              _.put_(_.feat_(_.by_(this._)), this._)
-            )
-          );
-        }
-      },
-      map: {
-        configurable: true,
-        value (...f) {
-          return this.fold(
-            (p, [k, v]) => _.put_(p, {[k]: _.pipe_(...f)(k, v)})
-          );
-        }
-      },
-      copy: {
-        configurable: true,
-        get () {
-          return this.map((k, v) => _.have_(v, Object) ? _(v).copy._ : v);
-        }
-      },
-      turn: {
-        configurable: true,
-        get () {
-          return this.fold((p, [k, v]) => p.put({[v]: k}));
-        }
-      },
-      filter: {
-        configurable: true,
-        value (...f) {
-          return this.sets.endo(
-            s => s.filter(([k, v]) => _.pipe_(...f)(k, v))
-          )
-          .fold(
-            (p, [k, v]) => _.put_(p, {[k]: v}),
-          );
-        }
-      },
-      pick: {
-        configurable: true,
-        value (...h) {
-          return this.filter(({k}) => h.includes(k));
-        }
-      },
-      drop: {
-        configurable: true,
-        value (...h) {
-          return this.filter(({k}) => !h.includes(k));
-        }
-      }
-    })
-  });
-  _.put_(_.by_(_), {
-    Array: _.feat_(_.by_(_).Object, {
-      map: {
-        configurable: true,
-        value (...f) {
-          return this.endo(a => a.map(_.pipe_(...f)));
-        }
-      },
-      filter: {
-        configurable: true,
-        value (...f) {
-          return this.endo(a => a.filter(_.pipe_(...f)));
-        }
-      },
-      exist: {
-        configurable: true,
-        values (v) {
-          return this.peek.be(a => a.includes(v));
-        }
-      },
-      some: {
-        configurable: true,
-        values (...f) {
-          return this.peek.be(a => a.some(_.pipe_(...f)));
-        }
-      },
-      every: {
-        configurable: true,
-        values (...f) {
-          return this.peek.be(a => a.every(_.pipe_(...f)));
-        }
-      },
-      pushL: {
-        configurable: true,
-        value (...v) {
-          return this.peek.endo(t => t.flat.unshift(...v)).poke;
-        }
-      },
-      pushR: {
-        configurable: true,
-        value (...v) {
-          return this.unit.peek.endo(t => t.flat.push(...v)).poke;
-        }
-      },
-      popL: {
-        configurable: true,
-        get () {
-          return this.peek.endo(t => t.flat.shift());
-        }
-      },
-      popR: {
-        configurable: true,
-        get () {
-          return this.peek.endo(t => t.flat.pop());
-        }
-      },
-      splice: {
-        configurable: true,
-        value (...v) {
-          return this.endo(a => a.splice(...v));
-        }
-      },
-      flat: {
-        configurable: true,
-        get () {
-          return this.endo(a => a.flat());
-        }
-      },
-      foldL: {
-        configurable: true,
-        value (...f) {
-          return this.endo(a => a.reduce(_.pipe_(...f)));
-        }
-      },
-      foldR: {
-        configurable: true,
-        value (...f) {
-          return this.endo(a => a.reduceRight(_.pipe_(...f)));
-        }
-      },
-      adaptL: {
-        configurable: true,
-        value (...a) {
-          return this.endo(b => b.map(v => v == null ? a.shift() : v));
-        }
-      },
-      adaptR: {
-        configurable: true,
-        value (...a) {
-          return this.adaptL(...a.reverse());
-        }
-      },
-      adapt: {
-        configurable: true,
-        get () {
-          return this.adaptL;
-        }
-      },
-      adaptRight: {
-        configurable: true,
-        get () {
-          return this.adaptR;
-        }
-      }
-    }),
-    Function: _.feat_(_.by_(_).Object, {
-      by: {
-        configurable: true,
-        get () {
-          return this.endo(_.by_);
-        }
-      },
-      part: {
-        configurable: true,
-        value (...v) {
-          return _(v).fullen_
-          ? this.to(...v)
-          : (...w) => this.part(...v.adaptL(...w));
-        }
-      },
-      done: {
-        configurable: true,
-        value (...v) {
-          return this.cached === undefined
-          ? this.unit.put({cached: this.lift(f => f(...v))}).flat.cached
-          : this.cached;
-        }
-      },
-      redo: {
-        configurable: true,
-        value (...v) {
-          return this.unit.put({cached: undefined}).flat.done(...v);
-        }
-      },
-      lift: {
-        configurable: true,
-        value (...v) {
-          return this.endo(f => _.Cat(f(...v)));
-        }
-      },
-      flip: {
-        configurable: true,
-        value (...v) {
-          return this.endo(f => f(...v));
-        }
-      }
-    }),
-    Unit: _.feat_(_.by_(_).Category, {
-      flat: {
-        configurable: true,
-        get () {
-          return _.id_(this.struct({
-            name: "Category"
-          })["@"]);
-        }
-      },
-      flip: {
-        configurable: true,
-        value (...f) {
-          return this.flat.endo(...f);
-        }
-      },
-      flop: {
-        configurable: true,
-        value (...f) {
-          return this.flat.map(...f).unit;
-        }
-      },
-      unit: {
-        configurable: true,
-        get () {
-          return this;
-        }
-      },
-      peek: {
-        configurable: true,
-        get () {
-          return this.put({"<": this.flat});
-        }
-      },
-      poke: {
-        configurable: true,
-        get () {
-          return this.endo(t => t["<"]);
-        }
-      },
-      lift: {
-        configurable: true,
-        get () {
-          return _.by_(_).Category.call(this, "CoAlgebra");
-        }
-      },
-      _: {
-        configurable: true,
-        get () {
-          return this._._;
-        }
-      }
-    })
-  });
-  _.put_(_.by_(_), {
-    CoAlgebra: _.feat_(_.by_(_).Unit, {
-      flop: {
-        configurable: true,
-        value (...f) {
-          return this.flat.map(...f);
-        }
-      },
-      get: {
-        configurable: true,
-        value (...a) {
-          return a.reduce((p, k) => p._ == null ? undefined : _.Cat(p._[k]), this);
-        }
-      },
-      set: {
-        configurable: true,
-        value (v, ...a) {
-          return a.reduce((p, k) => p.put({[k]: v})[k], this);
-        }
-      },
-      put: {
-        configurable: true,
-        value (...o) {
-          return this.endo(t => _.put_(t, ...o));
-        }
-      },
-      delete: {
-        configurable: true,
-        value (...k) {
-          return this.read(_ => k.forEach(k => delete _._[k]));
-        }
-      },
-      _: {
-        configurable: true,
-        get () {
-          return this._._;
-        }
-      }
-    })
-  });
-
   Object.defineProperties(_.prototype, {
+    _: {
+      configurable: true,
+      get () {
+        return _.id(this["@"]);
+      }
+    },
+    lift: {
+      configurable: true,
+      value (f = _, ...v) {
+        return f(this, ...v);
+      }
+    },
+    flat: {
+      configurable: true,
+      value (f = _.id_, ...v) {
+        return this._ == null ? this._ : f(this._, ...v);
+      }
+    },
+    endo: {
+      configurable: true,
+      value (f, ...v) {
+        return this._ == null ? this : _(f(this._, ...v));
+      }
+    },
+    bind: {
+      configurable: true,
+      value (f, ...v) {
+        return this.lift(t => t.flat(f, ...v));
+      }
+    },
+    take: {
+      configurable: true,
+      value (f, ...v) {
+        return this.lift(t => ({a: f(t, ...v), s: t}).s);
+      }
+    },
+    ask: {
+      configurable: true,
+      value (x, y, n) {
+        return this.lift(t => x(t) ? y(t) : n(t));
+      }
+    },
+    is: {
+      configurable: true,
+      value (c, y, n) {
+        return this.ask(t => _.is_(t._) === c, y, n);
+      } 
+    },
+    reduce: {
+      configurable: true,
+      get () {
+        return this.fold;
+      }
+    },
+    fold: {
+      configurable: true,
+      value (f, ...v) {
+        return this.is(
+          Array,
+          ({_}) => _.reduce((p, c) => f(p, c, ...v)),
+          t => t._.reduce((p, [k, w]) => f(p, k, w, ...v))
+        );
+      }
+    },
+    map: {
+      configurable: true,
+      value (f, ...v) {
+        return this.is(
+          Array,
+          ({_}) => _.map((w, k) => f(w, k, ...v)),
+          t => t.fold((p, k, w) => p.put({[k]: f(k, w, ...v)}), t.other)
+        );
+      }
+    },
     each: {
       configurable: true,
-      value (f) {
-        return this.read(a => _(a).entries._.forEach(f));
-      }
-    },
-    $: {
-      configurable: true,
-      get () {
-        return new Proxy(this, {
-          get (_, k, r) {
-            return r[k]
-            ? r[k](_)
-            : (...v) => _.read(
-              o => o[k].constructor === Function
-              ? o[k].call(o, ...v)
-              : _.set(v.shift(), k, ...v)
-            ).$;
-          },
-          _ (_) {
-            return _._;
-          },
-          $ (_) {
-            return _;
-          }
-        });
-      }
-    },
-    define: {
-      configurable: true,
-      value (o = {}) {
-        return this.lift(Object.defineProperties, o);
-      }
-    },
-    from: {
-      configurable: true,
-      value (o = {}) {
-        return this.lift(
-          m => Object.create(
-            m.constructor.prototype,
-            Object.assign(o, {
-              constructor: {
-                configurable: true,
-                writable: true,
-                value: _.by_(o) === Object ? m.constructor : o.constructor
-              }
-            })
+      value (f, ...v) {
+        return this.take(
+          t => t.is(
+            Array,
+            ({_}) => _.forEach(([k, w]) => f(w, k, ...v)),
+            t => t.all.flat(a => a.forEach(([k, w]) => f(k, w, ...v)))
           )
         );
       }
     },
-    list: {
+    filter: {
       configurable: true,
-      get () {
-        return this.lift(
-          o => _(o.length)[""]
-          ? _(o).draw({length: _(o).keys._.length}).list._
-          : Array.from(o)
+      value (f, ...v) {
+        return this.is(
+          Array,
+          ({_}) => _.filter((w, k) => f(w, k, ...v)),
+          t => t.all.fold(
+            (p, k, w) => f(k, w, ...v) ? p.put({[k]: w}) : p,
+            t.other
+          )
         );
       }
     },
-    json: {
+    pick: {
       configurable: true,
-      get () {
-        return JSON.stringify(this._);
-      }
-    }
-  });
-
-  _(String).define({
-    json: {
-      configurable: true,
-      get () {
-        try {
-          return _(JSON.parse(this));
-        } catch (e) {
-          return _(this);
-        }
-      }
-    }
-  });
-
-  _(Number).define({
-    _ : {
-      configurable: true,
-      value (n, s = 1) {
-        return [...function* (v) {
-          yield v;
-          while (v + s <= n) {
-            yield v += s;
-          }
-        }(this.valueOf())];
-      }
-    }
-  });
-
-  _(this).is(Object).$(() => _(_).annex({
-    on: {
-      configurable: true,
-      value (d) {
-        _(d).give(this["@"].on.bind(this["@"]));
-        return this;
+      value (...k) {
+        return this.filter(h => k.includes(h));
       }
     },
-    once: {
+    drop: {
       configurable: true,
-      value (d) {
-        _(d).give(this["@"].once.bind(this["@"]));
-        return this;
+      value (...k) {
+        return this.filter(h => !k.includes(h));
+      }
+    },
+    swap: {
+      configurable: true,
+      get () {
+        return this.all.fold((p, [k, v]) => p.put({[v]: k}), this.other);
+      }
+    },
+    by: {
+      configurable: true,
+      get () {
+        return this.endo(
+          t => t.prototype ? t.prototype : Object.getPrototypeOf(t)
+        );
+      }
+    },
+    keys: {
+      configurable: true,
+      get () {
+        return this.endo(Object.keys);
+      }
+    },
+    vals: {
+      configurable: true,
+      get () {
+        return this.endo(Object.values);
+      }
+    },
+    all: {
+      configurable: true,
+      get () {
+        return this.endo(Object.entries);
+      }
+    },
+    put: {
+      configurable: true,
+      value (f, ...o) {
+        return this.endo(Object.assign, ...o);
+      }
+    },
+    struct: {
+      configurable: true,
+      value (o) {
+        return this.endo(Object.create, o);
+      }
+    },
+    make: {
+      configurable: true,
+      value (...o) {
+        return this.lift(t => t.put(t.flat(Object.create), ...o));
+      }
+    },
+    other: {
+      configurable: true,
+      get () {
+        return this.by.endo(Object.create);
+      }
+    },
+    copy: {
+      configurable: true,
+      get () {
+        return this.lift(t => t.other.put(t._))._;
+      }
+    },
+    clone: {
+      configurable: true,
+      get () {
+        return this.lift(
+          t => t.all.endo(a => a.reduce((p, [k, v]) => p.put({
+            [k]: (
+              v instanceof Object
+              ? _(v).clone._
+              : v
+            )
+          }), t.other))
+        );
       }
     }
-  }));
+  });
 
-  _(this).by._ === Object ? module.exports = _ : this._ = _;
+  _.to_ = (...f) => (...v) => f.reduceRight((a, m) => m(a))(...v);
+  _.id_ = v => v == null ? null : v.valueOf();
+  _.is_ = v => v == null ? null : v.constructor;
+  _._ = _.pair = function (R, L, K) {
+    return Object.create(_.pair.prototype, {
+      "@": {
+        configurable: true,
+        get () {
+          return {L, R};
+        }
+      },
+      "#": {
+        configurable: true,
+        get () {
+          return K;
+        }
+      }
+    });
+  };
+  _._.prototype = Object.create(_.prototype, {
+    constructor: {
+      configurable: true,
+      writable: true,
+      value: _.pair
+    },
+    L: {
+      configurable: true,
+      get () {
+        return this._.L;
+      }
+    },
+    R: {
+      configurable: true,
+      get () {
+        return this._.R;
+      }
+    },
+    K: {
+      configurable: true,
+      get () {
+        return this["#"];
+      }
+    },
+    update: {
+      configurable: true,
+      value (f = _.id_, ...v) {
+        return this.Lift(({L, R}) => _._(f(L(R), ...v), L));
+      }
+    },
+    final: {
+      configurable: true,
+      value (f = _.id_, ...v) {
+        return this.endo(({L, R}) => f(L(R)));
+      }
+    },
+    swap: {
+      configurable: true,
+      get () {
+        return this.bind(({R, L}) => _._(L, R));
+      }
+    },
+    done: {
+      configurable: true,
+      value (f = _.id_, ...v) {
+        return this.lift(
+          ({L, R, K}) => (
+            K == null
+            ? _(f(L(R))).flat(r => _._(r, L, r))
+            : _._(K, L, K)
+          )
+        );
+      }
+    },
+    redo: {
+      configurable: true,
+      value (f = _.id_, ...v) {
+        return this.lift(({L, R}) => _(f(L(R))).flat(r => _.pair(r, L, r)));
+      }
+    }
+  });
+
+  Object.assign(_.is_(this) === Object ? module.exports : this._, _);
 })();
